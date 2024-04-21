fb0d9501e331f24158519a5827abfbf6742a4215
Guy <ga327@sussex.ac.uk>
2024-04-21 23:47:17 +0100

    Move code from generate-plots.py to stats.py

--- Code/experiments/Co-compression/stats.py
+++ Code/experiments/Co-compression/stats.py
@@ -235,3 +235,17 @@ class Stats:
     #plt.show()
     plt.savefig(filename, dpi=150, bbox_inches='tight', pad_inches=0)
     plt.close()
+
+
+
+if __name__ == "__main__":
+    S = Stats('stats.json')
+    S.update_all_and_save()
+    #S.compute_similarity_matrix()
+    #S.draw_similarity_heatmap(sort_by='file size', filename='fig_co-compression_file_size.png')
+    #S.draw_similarity_heatmap(sort_by='median', filename='fig_co-compression_median.png')
+    #S.compute_cocomp_matrix()
+    #S.draw_cocomp_heatmap(sort_by='file size', filename='fig_cocomp_file_size.png')
+    #S.draw_cocomp_heatmap(sort_by='median', filename='fig_cocomp_median.png')
+    #S.print_most_least_similar()
+    S.draw_cocomp_performance(filename='fig_cocomp_performance.png')
