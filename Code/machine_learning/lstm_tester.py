59362995ffa431fb18c705044a9f48e29e008226
Guy <ga327@sussex.ac.uk>
2024-04-29 02:12:50 +0100

    Expand RNN context, refactor

--- Code/machine_learning/lstm_tester.py
+++ Code/machine_learning/lstm_tester.py
@@ -82,27 +82,39 @@ class LSTMPredictor(Predictor):
 
 #############################################################
 
-# Loading a pre-trained Predictor and creating the corresponding Compressor
+if __name__ == "__main__":
 
-lstm_predictor  = LSTMPredictor(None, window=10)
-lstm_predictor.load('lstm-10')
-lstm_compressor = Compressor(predictor = lstm_predictor, Code = Arithmetic)
+    # Loading a pre-trained Predictor and creating the corresponding Compressor
 
-#############################################################
+    lstm_predictor  = LSTMPredictor(None, window=10)
+    lstm_predictor.load('lstm-10')
+    lstm_compressor = Compressor(predictor = lstm_predictor, Code = Arithmetic)
 
-# Encoding-decoding test
 
-inigo_text     = b'Hello. My name is Inigo Montoya. You killed my father. Prepare to die.'
-inigo_encoding = lstm_compressor.encode(inigo_text)
-inigo_decoding = lstm_compressor.decode(inigo_encoding)
+    # Encoding-decoding test
 
-print(f'Original text: {inigo_text}')
-print(f'Encoded text : {inigo_encoding.hex()}')
-print(f'Decoded text : {inigo_decoding}')
+    inigo_text     = b'Hello. My name is Inigo Montoya. You killed my father. Prepare to die.'
+    inigo_encoding = lstm_compressor.encode(inigo_text)
+    inigo_decoding = lstm_compressor.decode(inigo_encoding)
 
-#############################################################
+    print(f'Original text: {inigo_text}')
+    print(f'Encoded text : {inigo_encoding.hex()}')
+    print(f'Decoded text : {inigo_decoding}')
+    
 
-# Decoding noise to generate completions
+    # Decoding noise to generate completions
 
-print('\nThe pre-trained LSTM predictor generates the following example completion for the word "Elizabeth":')
-print(lstm_compressor.decode(randbytes(50)+b'\0', context=b'Elizabeth ').decode('utf8').__repr__())
+    print('\nThe pre-trained LSTM predictor generates the following example completion for the word "Elizabeth":')
+    print(lstm_compressor.decode(randbytes(50)+b'\0', context=b'Elizabeth ').decode('utf8').__repr__())
+    
+
+    # Estimating compression ratio on unseen data
+    
+    lstm_huffman_compressor = Compressor(predictor = lstm_predictor, Code = Huffman)
+
+    with open('../../Data/books/pg1661.txt','rb+') as f:
+      test_text = f.read()[10000:11000]
+      original_length = len(test_text)
+      compressed_text = lstm_huffman_compressor.encode(test_text)
+      compressed_length = len(compressed_text)
+      print(f'\nEstimated compression ratio: {original_length/compressed_length}')
--- Code/machine_learning/rnn_tester.py
+++ Code/machine_learning/rnn_tester.py
@@ -82,27 +82,39 @@ class RNNPredictor(Predictor):
 
 #############################################################
 
-# Loading a pre-trained Predictor and creating the corresponding Compressor
+if __name__ == "__main__":
 
-rnn_predictor  = RNNPredictor(None, window=5)
-rnn_predictor.load('rnn-6')
-rnn_compressor = Compressor(predictor = rnn_predictor, Code = Arithmetic)
+    # Loading a pre-trained Predictor and creating the corresponding Compressor
 
-#############################################################
+    rnn_predictor  = RNNPredictor(None, window=10)
+    rnn_predictor.load('rnn-10')
+    rnn_compressor = Compressor(predictor = rnn_predictor, Code = Arithmetic)
 
-# Encoding-decoding test
 
-inigo_text     = b'Hello. My name is Inigo Montoya. You killed my father. Prepare to die.'
-inigo_encoding = rnn_compressor.encode(inigo_text)
-inigo_decoding = rnn_compressor.decode(inigo_encoding)
+    # Encoding-decoding test
 
-print(f'Original text: {inigo_text}')
-print(f'Encoded text : {inigo_encoding.hex()}')
-print(f'Decoded text : {inigo_decoding}')
+    inigo_text     = b'Hello. My name is Inigo Montoya. You killed my father. Prepare to die.'
+    inigo_encoding = rnn_compressor.encode(inigo_text)
+    inigo_decoding = rnn_compressor.decode(inigo_encoding)
 
-#############################################################
+    print(f'Original text: {inigo_text}')
+    print(f'Encoded text : {inigo_encoding.hex()}')
+    print(f'Decoded text : {inigo_decoding}')
 
-# Decoding noise to generate completions
 
-print('\nThe pre-trained RNN predictor generates the following example completion for the word "Elizabeth":')
-print(rnn_compressor.decode(randbytes(50)+b'\0', context=b'Elizabeth ').decode('utf8').__repr__())
+    # Decoding noise to generate completions
+
+    print('\nThe pre-trained RNN predictor generates the following example completion for the word "Elizabeth":')
+    print(rnn_compressor.decode(randbytes(50)+b'\0', context=b'Elizabeth ').decode('utf8').__repr__())
+
+
+    # Estimating compression ratio on unseen data
+    
+    rnn_huffman_compressor = Compressor(predictor = rnn_predictor, Code = Huffman)
+
+    with open('../../Data/books/pg1661.txt','rb+') as f:
+      test_text = f.read()[10000:11000]
+      original_length = len(test_text)
+      compressed_text = rnn_huffman_compressor.encode(test_text)
+      compressed_length = len(compressed_text)
+      print(f'\nEstimated compression ratio: {original_length/compressed_length}')
