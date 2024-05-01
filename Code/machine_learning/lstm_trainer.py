84146da336e640e384bfd5f7894b7ac68371ac24
Guy <ga327@sussex.ac.uk>
2024-05-01 02:55:02 +0100

    Update LSTM weights

--- Code/machine_learning/lstm_trainer.py
+++ Code/machine_learning/lstm_trainer.py
@@ -1,16 +1,22 @@
-import os
+import os, numpy
 from lstm_tester import LSTMPredictor
 
 lstm_predictor  = LSTMPredictor(None, window=10)
 lstm_predictor.load('lstm-10')
 
 book_fnames = os.listdir('../../Data/books')
-book_fnames = ['pg1342.txt']
+#book_fnames = ['pg1342.txt']
 
 while True:
-    for fname in book_fnames:
-      print(f'Training on {fname}')
+    for fname in book_fnames[41:]:
       text = open('../../Data/books/'+fname,'rb+').read()
-      lstm_predictor.train(text)
-      print('Saving')
-      lstm_predictor.save('lstm-10')
+      if not b'Language: English' in text:
+        print(f'Skipping {fname}')
+        continue
+      try:
+        print(f'Training on {fname}')
+        lstm_predictor.train(text)
+        print('Saving')
+        lstm_predictor.save('lstm-10')
+      except numpy.core._exceptions._ArrayMemoryError:
+        continue
